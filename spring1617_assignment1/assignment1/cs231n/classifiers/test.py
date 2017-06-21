34df2e092307e0bfe4d84667658d3371983163f9
mindoool <min3482@snu.ac.kr>
2017-06-22 01:51:13 +0900

    SVM vectorized loss 까지 진행

--- spring1617_assignment1/assignment1/cs231n/classifiers/test.py
+++ spring1617_assignment1/assignment1/cs231n/classifiers/test.py
@@ -11,3 +11,12 @@ c = np.argmax(np.bincount(np.array([0, 1, 1, 3, 2, 1, 7])))
 print(a)
 print(b)
 print(c)
+
+print(x[1,1])
+print(x[1,2])
+
+d = np.array([[1,2,3], [4,5,6], [7,8,9]])
+print(np.reshape(d, (3,-1)))
+
+test = np.maximum(0, [1,2,3,-1])
+print(test)
